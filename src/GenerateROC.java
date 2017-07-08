413eaf7442651a7bd8a99d21fee1530ca0278286
Joseph Mammo <contact@josephmammo.com>
2017-07-08 14:52:36 -0500

    initial_working_commit
    Implemented the following:
    
    SelectAttributes.java
    *ReliefFAttributeEval
    *CorrelationAttributeEval
    *SymmetricalUncertAttributeEval
    *InfoGainAttributeEval
    *GainRatioAttributeEval
    
    GenerateROC.java
    *NaiveBayes
    *RandomForest
    *RandomTree

---
+++ src/GenerateROC.java
@@ -0,0 +1,193 @@
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.ThresholdCurve;
+import weka.classifiers.trees.RandomForest;
+import weka.classifiers.trees.RandomTree;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.gui.visualize.JComponentWriter;
+import weka.gui.visualize.JPEGWriter;
+import weka.gui.visualize.PlotData2D;
+import weka.gui.visualize.ThresholdVisualizePanel;
+
+import java.io.File;
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+/*
+ * @author Joseph Mammo	
+ * @version 003
+ */
+public class GenerateROC {
+
+	/**
+	 * Takes one argument: dataset in ARFF format (expects class to be last
+	 * attribute).
+	 * 
+	 * @param args
+	 *            the commandline arguments
+	 * @throws Exception
+	 *             if something goes wrong
+	 */
+	public static void main(String[] args) throws Exception {
+		// load data
+		Instances data = DataSource
+				.read("C:\\Users\\\\Joe\\eclipse-workspace\\WekaMLWorkbench\\output\\FeatureSelected\\Correlation.csv"); // args[0]
+		data.setClassIndex(data.numAttributes() - 1);
+
+		int n = data.numClasses();
+		for (int i = 0; i < n; i++) {
+			RandomForest(data, 10, i);
+			RandomTree(data, 10, i);
+		}
+		for (int i = 0; i < n; i++) {
+			NaiveBayes(data, 10, i);
+		}
+		System.exit(1);
+	}
+
+	public static void RandomForest(Instances data, int CrossValidation, int classIndex) throws Exception {
+		System.out.println("Analyzing class: " + classIndex + " using RandomForest classifier ...");
+
+		// evaluate classifier
+		// Classifier cl = new NaiveBayes();
+		Classifier cl = new RandomForest();
+		Evaluation eval = new Evaluation(data);
+		eval.crossValidateModel(cl, data, CrossValidation, new Random(1));
+
+		// generate curve
+		ThresholdCurve tc = new ThresholdCurve();
+		// int classIndex = 2;
+		Instances curve = tc.getCurve(eval.predictions(), classIndex);
+
+		// plot curve
+		ThresholdVisualizePanel tvp = new ThresholdVisualizePanel();
+		tvp.setROCString("(Area under ROC = " + Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4) + ")");
+		tvp.setName(curve.relationName());
+		PlotData2D plotdata = new PlotData2D(curve);
+		plotdata.setPlotName(curve.relationName());
+		plotdata.addInstanceNumberAttribute();
+		// specify which points are connected
+		boolean[] cp = new boolean[curve.numInstances()];
+		for (int n = 1; n < cp.length; n++)
+			cp[n] = true;
+		plotdata.setConnectPoints(cp);
+		// add plot
+		tvp.addPlot(plotdata);
+
+		// System.out.println(plotdata.getPlotInstances());
+
+		JFrame jf = new JFrame();
+		jf.setSize(900, 700);
+		jf.getContentPane().add(tvp);
+		jf.pack();
+
+		// Save to file specified as second argument (can use any of
+		// BMPWriter, JPEGWriter, PNGWriter, PostscriptWriter for different formats)
+		JComponentWriter jcw = new JPEGWriter(tvp.getPlotPanel(), new File("output/ROC/" + tvp.getName() + "_RF_"
+				+ classIndex + "_" + Utils.doubleToString(ThresholdCurve.getPRCArea(curve), 4) + "_.jpg"));
+		jcw.toOutput();
+
+		System.out.println();
+		System.out.println("(Area under PRC = " + Utils.doubleToString(ThresholdCurve.getPRCArea(curve), 4) + ")");
+		System.out.println("(Area under ROC = " + Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4) + ")");
+		System.out.println("Done!");
+	}
+
+	public static void NaiveBayes(Instances data, int CrossValidation, int classIndex) throws Exception {
+		System.out.println("Analyzing class: " + classIndex + " using NaiveBayes classifier ...");
+
+		// evaluate classifier
+		Classifier cl = new NaiveBayes();
+		Evaluation eval = new Evaluation(data);
+		eval.crossValidateModel(cl, data, CrossValidation, new Random(1));
+
+		// generate curve
+		ThresholdCurve tc = new ThresholdCurve();
+		// int classIndex = 2;
+		Instances curve = tc.getCurve(eval.predictions(), classIndex);
+
+		// plot curve
+		ThresholdVisualizePanel tvp = new ThresholdVisualizePanel();
+		tvp.setROCString("(Area under ROC = " + Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4) + ")");
+		tvp.setName(curve.relationName());
+		PlotData2D plotdata = new PlotData2D(curve);
+		plotdata.setPlotName(curve.relationName());
+		plotdata.addInstanceNumberAttribute();
+		// specify which points are connected
+		boolean[] cp = new boolean[curve.numInstances()];
+		for (int n = 1; n < cp.length; n++)
+			cp[n] = true;
+		plotdata.setConnectPoints(cp);
+		// add plot
+		tvp.addPlot(plotdata);
+
+		// System.out.println(plotdata.getPlotInstances());
+
+		JFrame jf = new JFrame();
+		jf.setSize(900, 700);
+		jf.getContentPane().add(tvp);
+		jf.pack();
+
+		// Save to file specified as second argument (can use any of
+		// BMPWriter, JPEGWriter, PNGWriter, PostscriptWriter for different formats)
+		JComponentWriter jcw = new JPEGWriter(tvp.getPlotPanel(), new File("output/ROC/" + tvp.getName() + "_NB_"
+				+ classIndex + "_" + Utils.doubleToString(ThresholdCurve.getPRCArea(curve), 4) + "_.jpg"));
+		jcw.toOutput();
+
+		System.out.println();
+		System.out.println("(Area under PRC = " + Utils.doubleToString(ThresholdCurve.getPRCArea(curve), 4) + ")");
+		System.out.println("(Area under ROC = " + Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4) + ")");
+		System.out.println("Done!");
+	}
+
+	public static void RandomTree(Instances data, int CrossValidation, int classIndex) throws Exception {
+		System.out.println("Analyzing class: " + classIndex + " using RandomTree classifier ...");
+
+		// evaluate classifier
+		Classifier cl = new RandomTree();
+		Evaluation eval = new Evaluation(data);
+		eval.crossValidateModel(cl, data, CrossValidation, new Random(1));
+
+		// generate curve
+		ThresholdCurve tc = new ThresholdCurve();
+		// int classIndex = 2;
+		Instances curve = tc.getCurve(eval.predictions(), classIndex);
+
+		// plot curve
+		ThresholdVisualizePanel tvp = new ThresholdVisualizePanel();
+		tvp.setROCString("(Area under ROC = " + Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4) + ")");
+		tvp.setName(curve.relationName());
+		PlotData2D plotdata = new PlotData2D(curve);
+		plotdata.setPlotName(curve.relationName());
+		plotdata.addInstanceNumberAttribute();
+		// specify which points are connected
+		boolean[] cp = new boolean[curve.numInstances()];
+		for (int n = 1; n < cp.length; n++)
+			cp[n] = true;
+		plotdata.setConnectPoints(cp);
+		// add plot
+		tvp.addPlot(plotdata);
+
+		// System.out.println(plotdata.getPlotInstances());
+
+		JFrame jf = new JFrame();
+		jf.setSize(900, 700);
+		jf.getContentPane().add(tvp);
+		jf.pack();
+
+		// Save to file specified as second argument (can use any of
+		// BMPWriter, JPEGWriter, PNGWriter, PostscriptWriter for different formats)
+		JComponentWriter jcw = new JPEGWriter(tvp.getPlotPanel(), new File("output/ROC/" + tvp.getName() + "_RT_"
+				+ classIndex + "_" + Utils.doubleToString(ThresholdCurve.getPRCArea(curve), 4) + "_.jpg"));
+		jcw.toOutput();
+
+		System.out.println();
+		System.out.println("(Area under PRC = " + Utils.doubleToString(ThresholdCurve.getPRCArea(curve), 4) + ")");
+		System.out.println("(Area under ROC = " + Utils.doubleToString(ThresholdCurve.getROCArea(curve), 4) + ")");
+		System.out.println("Done!");
+	}
+}
---
+++ src/SelectAttributes.java
@@ -0,0 +1,282 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import weka.attributeSelection.ReliefFAttributeEval;
+import weka.attributeSelection.AttributeSelection;
+import weka.attributeSelection.CorrelationAttributeEval;
+import weka.attributeSelection.SymmetricalUncertAttributeEval;
+import weka.attributeSelection.InfoGainAttributeEval;
+import weka.attributeSelection.GainRatioAttributeEval;
+import weka.attributeSelection.Ranker;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+
+/*
+ * @author Joseph Mammo	
+ * @version 004
+ */
+public class SelectAttributes {
+
+	/**
+	 * Takes one argument: dataset in ARFF format (expects class to be last
+	 * attribute).
+	 * 
+	 * @param args
+	 *            the commandline arguments
+	 * @throws Exception
+	 *             if something goes wrong
+	 */
+	public static void main(String[] args) throws Exception {
+		// load data
+		System.out.println("\n0. Loading data ...");
+		DataSource source = new DataSource("C:\\Users\\Joe\\Desktop\\Summer 2017\\BioSNTR\\datasets\\V1V3.csv");
+		Instances data = source.getDataSet();
+		if (data.classIndex() == -1)
+			data.setClassIndex(data.numAttributes() - 1);
+		// process data
+		System.out.println("\n1. Processing data ...");
+
+		ReliefFAttributeEval(data);
+		CorrelationAttributeEval(data);
+		SymmetricalUncertAttributeEval(data);
+		InfoGainAttributeEval(data);
+		GainRatioAttributeEval(data);
+	}
+
+	protected static void ReliefFAttributeEval(Instances data) throws Exception {
+		System.out.println("\n Selecting Features ...");
+		weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+
+		ReliefFAttributeEval ra = new ReliefFAttributeEval();
+		ra.setDoNotCheckCapabilities(false);
+		ra.setNumNeighbours(10);
+		ra.setSampleSize(-1);
+		ra.setSeed(1);
+		ra.setSigma(2);
+
+		Ranker rk = new Ranker();
+		rk.setGenerateRanking(true);
+		rk.setNumToSelect(-1);
+		rk.setThreshold(-1.7976931348623157E308);
+
+		filter.setEvaluator(ra);
+		filter.setSearch(rk);
+		filter.setInputFormat(data);
+		Instances newData = Filter.useFilter(data, filter);
+
+		// Select features
+		AttributeSelection attsel = new AttributeSelection();
+		attsel.setEvaluator(ra);
+		attsel.setSearch(rk);
+
+		attsel.setFolds(10);
+		attsel.setSeed(1);
+
+		attsel.SelectAttributes(data);
+		int[] indices = attsel.selectedAttributes();
+		System.out.println(attsel.toResultsString());
+
+		Object[] classes = new Object[indices.length];
+		for (int i = 0; i < indices.length - 1; i++) {
+			classes[i] = data.attribute(indices[i]).name();
+		}
+		classes[indices.length - 1] = "class"; // add class label so it is compatible to read back
+
+		// Write out selected features
+		writeFile(classes, newData, "ReliefF"); // class, data, fileName
+
+	}
+
+	protected static void CorrelationAttributeEval(Instances data) throws Exception {
+		System.out.println("\n Selecting Features ...");
+		weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+
+		CorrelationAttributeEval ca = new CorrelationAttributeEval();
+		ca.setDoNotCheckCapabilities(false);
+		ca.setOutputDetailedInfo(false);
+
+		Ranker rk = new Ranker();
+		rk.setGenerateRanking(true);
+		rk.setNumToSelect(-1);
+		rk.setThreshold(-1.7976931348623157E308);
+
+		filter.setEvaluator(ca);
+		filter.setSearch(rk);
+		filter.setInputFormat(data);
+		Instances newData = Filter.useFilter(data, filter);
+
+		// Select features
+		AttributeSelection attsel = new AttributeSelection();
+		attsel.setEvaluator(ca);
+		attsel.setSearch(rk);
+
+		attsel.setFolds(10);
+		attsel.setSeed(1);
+
+		attsel.SelectAttributes(data);
+		int[] indices = attsel.selectedAttributes();
+		System.out.println(attsel.toResultsString());
+
+		Object[] classes = new Object[indices.length];
+		for (int i = 0; i < indices.length - 1; i++) {
+			classes[i] = data.attribute(indices[i]).name();
+		}
+		classes[indices.length - 1] = "class"; // add class label so it is compatible to read back
+
+		// Write out selected features
+		writeFile(classes, newData, "Correlation"); // class, data, fileName
+
+	}
+
+	protected static void SymmetricalUncertAttributeEval(Instances data) throws Exception {
+		System.out.println("\n Selecting Features ...");
+		weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+
+		SymmetricalUncertAttributeEval su = new SymmetricalUncertAttributeEval();
+		su.setDoNotCheckCapabilities(false);
+		su.setMissingMerge(true);
+
+		Ranker rk = new Ranker();
+		rk.setGenerateRanking(true);
+		rk.setNumToSelect(-1);
+		rk.setThreshold(-1.7976931348623157E308);
+
+		filter.setEvaluator(su);
+		filter.setSearch(rk);
+		filter.setInputFormat(data);
+		Instances newData = Filter.useFilter(data, filter);
+
+		// Select features
+		AttributeSelection attsel = new AttributeSelection();
+		attsel.setEvaluator(su);
+		attsel.setSearch(rk);
+
+		attsel.setFolds(10);
+		attsel.setSeed(1);
+
+		attsel.SelectAttributes(data);
+		int[] indices = attsel.selectedAttributes();
+		System.out.println(attsel.toResultsString());
+
+		Object[] classes = new Object[indices.length];
+		for (int i = 0; i < indices.length - 1; i++) {
+			classes[i] = data.attribute(indices[i]).name();
+		}
+		classes[indices.length - 1] = "class"; // add class label so it is compatible to read back
+
+		// Write out selected features
+		writeFile(classes, newData, "SymmetricalUncert"); // class, data, fileName
+
+	}
+
+	protected static void InfoGainAttributeEval(Instances data) throws Exception {
+		System.out.println("\n Selecting Features ...");
+		weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+
+		InfoGainAttributeEval ig = new InfoGainAttributeEval();
+		ig.setBinarizeNumericAttributes(false);
+		ig.setDoNotCheckCapabilities(false);
+		ig.setMissingMerge(true);
+
+		Ranker rk = new Ranker();
+		rk.setGenerateRanking(true);
+		rk.setNumToSelect(-1);
+		rk.setThreshold(-1.7976931348623157E308);
+
+		filter.setEvaluator(ig);
+		filter.setSearch(rk);
+		filter.setInputFormat(data);
+		Instances newData = Filter.useFilter(data, filter);
+
+		// Select features
+		AttributeSelection attsel = new AttributeSelection();
+		attsel.setEvaluator(ig);
+		attsel.setSearch(rk);
+
+		attsel.setFolds(10);
+		attsel.setSeed(1);
+
+		attsel.SelectAttributes(data);
+		int[] indices = attsel.selectedAttributes();
+		System.out.println(attsel.toResultsString());
+
+		Object[] classes = new Object[indices.length];
+		for (int i = 0; i < indices.length - 1; i++) {
+			classes[i] = data.attribute(indices[i]).name();
+		}
+		classes[indices.length - 1] = "class"; // add class label so it is compatible to read back
+
+		// Write out selected features
+		writeFile(classes, newData, "InfoGain"); // class, data, fileName
+
+	}
+
+	protected static void GainRatioAttributeEval(Instances data) throws Exception {
+		System.out.println("\n Selecting Features ...");
+		weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+
+		GainRatioAttributeEval gr = new GainRatioAttributeEval();
+		gr.setDoNotCheckCapabilities(false);
+		gr.setMissingMerge(true);
+
+		Ranker rk = new Ranker();
+		rk.setGenerateRanking(true);
+		rk.setNumToSelect(-1);
+		rk.setThreshold(-1.7976931348623157E308);
+
+		filter.setEvaluator(gr);
+		filter.setSearch(rk);
+		filter.setInputFormat(data);
+		Instances newData = Filter.useFilter(data, filter);
+
+		// Select features
+		AttributeSelection attsel = new AttributeSelection();
+		attsel.setEvaluator(gr);
+		attsel.setSearch(rk);
+
+		attsel.setFolds(10);
+		attsel.setSeed(1);
+
+		attsel.SelectAttributes(data);
+		int[] indices = attsel.selectedAttributes();
+		System.out.println(attsel.toResultsString());
+
+		Object[] classes = new Object[indices.length];
+		for (int i = 0; i < indices.length - 1; i++) {
+			classes[i] = data.attribute(indices[i]).name();
+		}
+		classes[indices.length - 1] = "class"; // add class label so it is compatible to read back
+
+		// Write out selected features
+		writeFile(classes, newData, "GainRatio"); // class, data, fileName
+
+	}
+
+	public static void writeFile(Object[] classes, Instances data, String fileName) throws IOException {
+		ArrayList<String> lines = new ArrayList<>();
+
+		String[] tempData = asStrings(data.toArray());
+		String[] tempClassStArr = asStrings(classes);
+
+		lines.add(String.join(",", tempClassStArr));
+
+		for (int i = 0; i < tempData.length; i++) {
+			lines.add(tempData[i]);
+		}
+
+		Path file = Paths.get("output/FeaturesSelected/" + fileName + ".csv");
+		Files.write(file, lines, Charset.forName("UTF-8"));
+	}
+
+	public static String[] asStrings(Object... objArray) {
+		String[] strArray = new String[objArray.length];
+		for (int i = 0; i < objArray.length; i++)
+			strArray[i] = String.valueOf(objArray[i]);
+		return strArray;
+	}
+
+}
